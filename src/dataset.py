8399459b3a311fab46d6e6a90e2c78bf09f94981
Le Hong Phuc <lehongphucnasa@gmail.com>
2026-07-11 00:13:59 +0700

    fix: path error:

--- src/dataset.py
+++ src/dataset.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from PIL import Image
 from torch.utils.data import Dataset
-
+from pathlib import Path
 class BottleDataset(Dataset):
     def __init__(self, manifest_df: pd.DataFrame, transform=None):
         self.df = manifest_df.reset_index(drop=True)
@@ -12,7 +12,8 @@ class BottleDataset(Dataset):
 
     def __getitem__(self, idx):
         row = self.df.iloc[idx]
-        img = Image.open(row.filepath).convert("RGB")
+        filepath = Path(row.filepath.replace("\\", "/"))
+        img = Image.open(filepath).convert("RGB")
         if self.transform:
             img = self.transform(img)
         return img, int(row.label)
